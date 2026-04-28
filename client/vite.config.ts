03dd53387e5a4410b8ebec8be6824a79e6db8833
Emmanuel <emlopezdev@gmail.com>
2026-04-27 21:13:44 -0400

    configure for vercel deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     base: "/",
+    resolve: {
+        alias: {
+            "@lucid/types": path.resolve(__dirname, "../packages/types/index.ts"),
+        },
+    },
     build: {
         outDir: "dist",
-        emptyOutDir: true, // Clean the dist folder before building
+        emptyOutDir: true,
     },
 });
