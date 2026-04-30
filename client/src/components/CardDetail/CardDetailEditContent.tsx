bdf71b1fcc615e57f2e36a49f5a3db3967b1163b
Emmanuel <emlopezdev@gmail.com>
2026-04-30 13:02:06 -0400

    fix(CardDetail): Updated to be more responsive to screen size

--- client/src/components/CardDetail/CardDetailEditContent.tsx
+++ client/src/components/CardDetail/CardDetailEditContent.tsx
@@ -81,26 +81,24 @@ const CardDetailEditContent = ({ data, onSubmit, onCancel }: CardDetailEditConte
                         onChange={onStatusSelect}
                         selectSize="small"
                     />
-                    <>
-                        <Input
-                            label="Price"
-                            type="number"
-                            onChange={() => {}}
-                            inputSize="small"
-                            hasErrorText={false}
-                            placeholder="0.00"
-                            min="0"
-                            step="0.01"
-                        />
-                        <Input
-                            label="Purchase Date"
-                            type="date"
-                            value={data.date_purchased?.toISOString().split("T")[0] ?? ""}
-                            onChange={() => {}}
-                            inputSize="small"
-                            hasErrorText={false}
-                        />
-                    </>
+                    <Input
+                        label="Price"
+                        type="number"
+                        onChange={() => {}}
+                        inputSize="small"
+                        hasErrorText={false}
+                        placeholder="0.00"
+                        min="0"
+                        step="0.01"
+                    />
+                    <Input
+                        label="Purchase Date"
+                        type="date"
+                        value={data.date_purchased?.toISOString().split("T")[0] ?? ""}
+                        onChange={() => {}}
+                        inputSize="small"
+                        hasErrorText={false}
+                    />
                     <Input
                         label="Hours"
                         type="number"
@@ -109,6 +107,7 @@ const CardDetailEditContent = ({ data, onSubmit, onCancel }: CardDetailEditConte
                         inputSize="small"
                         hasErrorText={false}
                         min="0"
+                        step="1"
                     />
                     <Input
                         label="Rating"
