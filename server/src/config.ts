aac4f1cb5784749cdbcc815e3ec3ccd21e1259b1
Emmanuel <emlopezdev@gmail.com>
2026-05-02 15:41:07 -0400

    fix: updated cors allowed origins instead of wildcard

--- server/src/config.ts
+++ server/src/config.ts
@@ -5,6 +5,7 @@ dotenv.config();
 const config = {
     PORT: process.env.PORT || 8000,
     MONGO_URL: process.env.MONGO_URL || "",
+    ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS?.split(",") ?? ["http://localhost:5173"],
 };
 
 export default config;
