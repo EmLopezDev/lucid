1bd0a10d8d452791b780248eb697f47aaea6d549
Emmanuel <emlopezdev@gmail.com>
2026-05-06 12:14:30 -0400

    fix(bcrypt): Argon2 is incompatible with Vercel had to switch to bcrypt

--- server/src/models/auth/auth.model.ts
+++ server/src/models/auth/auth.model.ts
@@ -1,4 +1,4 @@
-import * as argon from "argon2";
+import bcrypt from "bcryptjs";
 import { AuthModel } from "./auth.mongo";
 
 type AuthCredentialsType = {
@@ -6,26 +6,20 @@ type AuthCredentialsType = {
     password: string;
 };
 
-export const registerAuthCredential = async (
-    credentials: AuthCredentialsType,
-) => {
+export const registerAuthCredential = async (credentials: AuthCredentialsType) => {
     const { id, password } = credentials;
-
-    const hash = await argon.hash(password);
+    const hash = await bcrypt.hash(password, 12);
     return await AuthModel.create({ user_id: id, hash });
 };
 
-export const signInAuthCredentials = async (
-    credentials: AuthCredentialsType,
-) => {
+export const signInAuthCredentials = async (credentials: AuthCredentialsType) => {
     const { id, password } = credentials;
     const userCredentials = await AuthModel.findOne({ user_id: id });
     if (!userCredentials) {
         throw new Error("Unable to sign in, please try again");
     }
 
-    const validatePassword = await argon.verify(userCredentials.hash, password);
-
+    const validatePassword = await bcrypt.compare(password, userCredentials.hash);
     if (!validatePassword) {
         throw new Error("One or more credentials is incorrect");
     }
