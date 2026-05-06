b1b526588b4700c89185b479e98a135835da0779
Emmanuel <emlopezdev@gmail.com>
2026-05-06 12:04:00 -0400

    fix: moved server deps to root for vercel

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,22 @@
+import type { IncomingMessage, ServerResponse } from "http";
 import mongoose from "mongoose";
 import app from "../server/src/app";
 import { mongoConnect } from "../server/src/services/mongo";
 
-if (mongoose.connection.readyState === 0) {
-    mongoConnect();
-}
+let connectionPromise: Promise<void> | null = null;
 
-export default app;
+const ensureConnected = () => {
+    if (mongoose.connection.readyState === 1) return Promise.resolve();
+    if (!connectionPromise) {
+        connectionPromise = mongoConnect().catch((err) => {
+            connectionPromise = null;
+            throw err;
+        });
+    }
+    return connectionPromise;
+};
+
+export default async function handler(req: IncomingMessage, res: ServerResponse) {
+    await ensureConnected();
+    app(req, res);
+}
