74661fbea6907b63c45316008a32506177ac5de6
Emmanuel <emlopezdev@gmail.com>
2026-05-06 11:54:23 -0400

    fix(mongoDB): mongoDB needs to connect in api/

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,9 @@
+import mongoose from "mongoose";
 import app from "../server/src/app";
+import { mongoConnect } from "../server/src/services/mongo";
+
+if (mongoose.connection.readyState === 0) {
+    mongoConnect();
+}
+
 export default app;
