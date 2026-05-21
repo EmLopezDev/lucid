791b1277a7188cd2a45e86fa71192cb3ee1a9bbd
Emmanuel <emlopezdev@gmail.com>
2026-05-21 17:16:55 -0400

    fix: allow media.rawg.io in client CSP for cover images

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -15,7 +15,7 @@ function cspPlugin() {
                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
                 "font-src 'self' https://fonts.gstatic.com",
                 "connect-src 'self' https://sentry.io https://*.sentry.io",
-                "img-src 'self' data: https://cdn.cloudflare.steamstatic.com https://cdn.steamstatic.com",
+                "img-src 'self' data: https://cdn.cloudflare.steamstatic.com https://cdn.steamstatic.com https://media.rawg.io",
                 "object-src 'none'",
                 "base-uri 'self'",
                 "form-action 'self'",
