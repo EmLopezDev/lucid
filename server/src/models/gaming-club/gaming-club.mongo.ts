49501c8d07dd079490f05db4bda58bc31a27f1e2
Emmanuel <emlopezdev@gmail.com>
2026-06-04 11:56:08 -0400

    feat: added mongoose Schema/Model for GamingClub feature

---
+++ server/src/models/gaming-club/gaming-club.mongo.ts
@@ -0,0 +1,58 @@
+import { Schema, model } from "mongoose";
+import { type GamingClubType } from "../../../../packages/types/GamingClubTypes";
+
+type GamingClubDocument = Omit<
+    GamingClubType,
+    "current_game" | "past_games" | "created_at" | "updated_at" | "deleted_at"
+> & {
+    current_game: {
+        title: string;
+        cover_url: string | null;
+        start_date: Date | null;
+        end_date: Date | null;
+    } | null;
+    past_games: Array<{
+        title: string;
+        cover_url: string | null;
+        end_date: Date;
+    }>;
+    created_at: Date;
+    updated_at: Date | null;
+    deleted_at: Date | null;
+};
+
+const GamingClubSchema = new Schema<GamingClubDocument>(
+    {
+        name: { type: String, required: true },
+        owner: { type: String, required: true },
+        avatar_url: { type: String, default: null },
+        description: { type: String, default: null },
+        members: [{ type: String }],
+        visibility: { type: String, required: true },
+        invite_code: { type: String, default: null },
+        current_game: {
+            type: {
+                title: { type: String, required: true },
+                cover_url: { type: String, default: null },
+                start_date: { type: Date, default: null },
+                end_date: { type: Date, default: null },
+            },
+            default: null,
+        },
+        past_games: [
+            {
+                type: {
+                    title: { type: String, required: true },
+                    cover_url: { type: String, default: null },
+                    end_date: { type: Date, required: true },
+                },
+            },
+        ],
+        created_at: { type: Date, required: true },
+        updated_at: { type: Date, default: null },
+        deleted_at: { type: Date, default: null },
+    },
+    { versionKey: false },
+);
+
+export const GamingClubModel = model<GamingClubDocument>("GamingClub", GamingClubSchema);
