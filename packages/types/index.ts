d851aa96b150f7f49d7b76c54d8f0e2e1dfeda43
Emmanuel <emlopezdev@gmail.com>
2026-06-09 21:43:23 -0400

    fix: rebase fix

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,7 +1,5 @@
 export * from "./AuthTypes";
-export * from "./GameSearch";
 export * from "./GamingClubTypes";
 export * from "./GamingClubPostTypes";
-export * from "./SelectOptionsTypes";
 export * from "./UserLibrary";
 export * from "./UserTypes";
